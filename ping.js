eb58c210a021a50cacb253d9e0c84a2cd94f79e7
amieldev <amielsentayehu@gmail.com>
2022-08-22 22:02:05 +0000

    Added ping to repo

--- ping.js
+++ ping.js
@@ -4,7 +4,7 @@ const chalk=require('chalk');
 const target=process.argv[2];
 
 function ping(url){    
-    axios.get(url)
+    axios.get(url,{timeout:10000})
       .then((response) => {
         console.log(`${chalk.blueBright(url)} is ${chalk.greenBright('up')}.`);
         ping(url);
