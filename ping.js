ddeb5787e4220d415361297b3f7ca79b755719ba
amieldev <amielsentayehu@gmail.com>
2022-08-22 22:34:44 +0000

    New update

--- ping.js
+++ ping.js
@@ -1,15 +1,25 @@
 const axios=require('axios');
 const chalk=require('chalk');
+const figlet=require('figlet');
+
+figlet('PingJS',function(err,data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(chalk.yellow(data));
+});
 
 const target=process.argv[2];
 
 function ping(url){    
     axios.get(url,{timeout:10000})
-      .then((response) => {
+      .then((response)=>{
         console.log(`${chalk.blueBright(url)} is ${chalk.greenBright('up')}.`);
         ping(url);
     })
-      .catch((error) => {
+      .catch((error)=>{
         console.log(`${chalk.blueBright(url)} is ${chalk.redBright('down')}.`);
         ping(url);
       });
