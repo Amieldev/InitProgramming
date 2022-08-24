387acea8d2ff7e97f4d25d4c76fd9ddaefb76f57
amieldev <amielsentayehu@gmail.com>
2022-08-24 15:46:58 +0000

    Deleted stuff

--- ping.js
+++ ping.js
@@ -14,11 +14,14 @@ figlet('PingJS',function(err,data) {
 const target=process.argv[2];
 
 function ping(url){    
+  if(!url.includes('http')){
+    url='https://'+url;
+  }    
     axios.get(url,{timeout:10000})
       .then((response)=>{
         console.log(`${chalk.blueBright(url)} is ${chalk.greenBright('up')}.`);
         ping(url);
-    })
+      })
       .catch((error)=>{
         console.log(`${chalk.blueBright(url)} is ${chalk.redBright('down')}.`);
         ping(url);
